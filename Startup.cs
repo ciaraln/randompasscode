cdcb814f2f2d284ca801922d69c86d71842217db
Ciara <ciara.l.nelson@live.com>
2018-04-09 19:06:19 -0700

    first commit add bootstrap later

---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+namespace randompasscode
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        services.AddSession();
+        services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseDeveloperExceptionPage();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "Default", // The route's name is only for our own reference
+                    template: "", // The pattern that the route matches
+                    defaults: new { controller = "Random", action = "/" } // The controller and method to execute
+                );
+            });
+            
+        }      
+    }    
+
+
+}
